c586b515d296d67b2e5ba7ec0b98046a443d886d
Scanner <scanner@apricot.com>
2013-08-28 17:51:23 -0700

    just put a placeholder file in for the arduino code.

---
+++ arduino/lumapad_eimp/lumapad_eimp.ino
@@ -0,0 +1,12 @@
+// NOTE: Just a place holder for the arduino code to let the electric
+//       imp control the lumapad.
+//
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
